70034e6a38d1e592441e1e78bbbdaf0ca718f25b
unknown <mchenf@icloud.com>
2020-09-24 01:41:11 +0800

    initial commit

---
+++ MomiToolset/Debugburr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MomiToolset
+{
+    public class Debugburr
+    {
+        public void Test()
+        {
+            Trace.Listeners.Clear();
+            string timeStamp = DateTime.Now.ToString("yyyy_MMdd_hhmmss_");
+            TextWriterTraceListener tl = new TextWriterTraceListener($".\\Logs\\{timeStamp}Log.log");
+            tl.TraceOutputOptions = TraceOptions.DateTime | TraceOptions.Callstack | TraceOptions.ProcessId;
+            Trace.Listeners.Add(tl);
+
+            Trace.WriteLine("Testing Starts");
+            Random rand = new Random();
+            Trace.Write("正在进行某种复杂运算");
+            for (int i = 0; i < 30; i++)
+            {
+                int SleepMS = rand.Next(10, 50);
+                Thread.Sleep(SleepMS);
+                Trace.Write(".");
+
+            }
+            Trace.WriteLine("完成！");
+            Trace.TraceWarning("某种警告！");
+
+
+
+            Trace.WriteLine("Testing Ends");
+            Trace.Close();
+        }
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MomiToolset;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debugburr b = new Debugburr();
+            b.Test();
+
+            Console.ReadLine();
+        }
+    }
+}
